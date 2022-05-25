2c88cc787bd28f83c7295d0a89d3b0791ba80b87
Joao <joaoclambert@hotmail.com>
2022-05-25 19:43:44 -0300

    Implementando controller

---
+++ src/main/java/com/desafio/beer/controller/BeerController.java
@@ -0,0 +1,45 @@
+package com.desafio.beer.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.desafio.beer.model.Beer;
+import com.desafio.beer.service.BeerService;
+
+@RestController
+@RequestMapping("beer")
+public class BeerController {
+
+	@Autowired
+	private BeerService beerService;
+	
+	@PostMapping
+	public Object criar(Beer beer) {
+		var criada = beerService.criar(beer);
+		return new ResponseEntity<>(criada, HttpStatus.OK);
+	}
+	
+	@GetMapping
+	public Object listar() {
+		var cervejas = beerService.listar();
+		return new ResponseEntity<>(cervejas, HttpStatus.OK);
+	}
+	
+	@GetMapping("{id}/precoPorLitro")
+	public Object precoPorLitro(Long id) {
+		var resultado = beerService.precoPorLitro(id);
+		return new ResponseEntity<>(resultado, HttpStatus.OK);
+	}
+	
+	@DeleteMapping
+	public Object deletar(Long id) {
+		beerService.deletar(id);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+}
