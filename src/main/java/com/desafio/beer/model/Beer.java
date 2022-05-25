4af97e51893d4bc11d14fa1095bd77b912e5d19e
Joao <joaoclambert@hotmail.com>
2022-05-25 19:00:56 -0300

    Configurando Model Cerveja

--- src/main/java/com/desafio/beer/model/Beer.java
+++ src/main/java/com/desafio/beer/model/Beer.java
@@ -1,6 +1,50 @@
 package com.desafio.beer.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Entity
 public class Beer {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Long id;
+
+	private String nome;
+	private double valor;
+	private int litrosEmbalagem;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+
+	public int getLitrosEmbalagem() {
+		return litrosEmbalagem;
+	}
+
+	public void setLitrosEmbalagem(int litrosEmbalagem) {
+		this.litrosEmbalagem = litrosEmbalagem;
+	}
 }
