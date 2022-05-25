30051966505feb21bf45db3f4c6a522ad652f340
Joao <joaoclambert@hotmail.com>
2022-05-25 19:29:48 -0300

    Criando service

--- src/main/java/com/desafio/beer/service/BeerServiceImpl.java
+++ src/main/java/com/desafio/beer/service/BeerServiceImpl.java
@@ -2,35 +2,42 @@ package com.desafio.beer.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.desafio.beer.model.Beer;
+import com.desafio.beer.repository.BeerRepository;
 
 @Service
 public class BeerServiceImpl implements BeerService {
+	
+	@Autowired
+	private BeerRepository beerRepository;
 
 	@Override
 	public Beer criar(Beer beer) {
-		// TODO Auto-generated method stub
-		return null;
+		Beer criada = beerRepository.save(beer);
+		
+		return criada;
 	}
 
 	@Override
 	public List<Beer> listar() {
-		// TODO Auto-generated method stub
-		return null;
+		return beerRepository.findAll();
 	}
 
 	@Override
 	public Double precoPorLitro(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		Beer cerveja = beerRepository.getById(id);
+		
+		double resultado = cerveja.getValor() / cerveja.getLitrosEmbalagem();
+		
+		return resultado;
 	}
 
 	@Override
 	public void deletar(Long id) {
-		// TODO Auto-generated method stub
-		
+		beerRepository.deleteById(id);
 	}
 
 }
