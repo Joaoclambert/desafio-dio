8957b1eb512c3e516c147303bcc7e1ec473a6d06
Joao <joaoclambert@hotmail.com>
2022-05-25 19:17:41 -0300

    Criando Service Implementation

---
+++ src/main/java/com/desafio/beer/service/BeerServiceImpl.java
@@ -0,0 +1,36 @@
+package com.desafio.beer.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.desafio.beer.model.Beer;
+
+@Service
+public class BeerServiceImpl implements BeerService {
+
+	@Override
+	public Beer criar(Beer beer) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Beer> listar() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Double precoPorLitro(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deletar(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
