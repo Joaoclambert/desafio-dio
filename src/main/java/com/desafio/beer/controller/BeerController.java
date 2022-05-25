2c9b476072001b943899c4394646fff96649fc5d
Joao <joaoclambert@hotmail.com>
2022-05-25 19:56:26 -0300

    Ajustando controller

--- src/main/java/com/desafio/beer/controller/BeerController.java
+++ src/main/java/com/desafio/beer/controller/BeerController.java
@@ -5,7 +5,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +22,7 @@ public class BeerController {
 	private BeerService beerService;
 	
 	@PostMapping
-	public Object criar(Beer beer) {
+	public Object criar(@RequestBody Beer beer) {
 		var criada = beerService.criar(beer);
 		return new ResponseEntity<>(criada, HttpStatus.OK);
 	}
@@ -32,13 +34,13 @@ public class BeerController {
 	}
 	
 	@GetMapping("{id}/precoPorLitro")
-	public Object precoPorLitro(Long id) {
+	public Object precoPorLitro(@PathVariable Long id) {
 		var resultado = beerService.precoPorLitro(id);
 		return new ResponseEntity<>(resultado, HttpStatus.OK);
 	}
 	
-	@DeleteMapping
-	public Object deletar(Long id) {
+	@DeleteMapping("{id}")
+	public Object deletar(@PathVariable Long id) {
 		beerService.deletar(id);
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
