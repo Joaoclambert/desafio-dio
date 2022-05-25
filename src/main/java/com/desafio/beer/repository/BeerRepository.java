c97ddb0301edb46cca1cd478bc535c3278f78d47
Joao <joaoclambert@hotmail.com>
2022-05-25 19:08:14 -0300

    Criando e configurando o repositorio

---
+++ src/main/java/com/desafio/beer/repository/BeerRepository.java
@@ -0,0 +1,11 @@
+package com.desafio.beer.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.desafio.beer.model.Beer;
+
+@Repository
+public interface BeerRepository extends JpaRepository<Beer, Long> {
+
+}
